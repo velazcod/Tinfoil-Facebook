785b25cdb4c69e40b048f6eccd399969631f87f6
Daniel Velazco <danny@greenvirtualsolutions.com>
2011-10-24 17:19:56 -0400

    First commit. Partially functional, need to do a preferences screen and clean up the code

---
+++ src/com/danvelazco/fbwrapper/Constants.java
@@ -0,0 +1,12 @@
+package com.danvelazco.fbwrapper;
+
+public class Constants {
+
+	public static final String TAG = "FB-Wrapper";
+	
+	public static final boolean PRODUCTION = false;
+	
+	//Google Chrome's user agent from a MacBook running 10.7.2 
+	public static final String USER_AGENT_DESKTOP = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1";
+	
+}
---
+++ src/com/danvelazco/fbwrapper/FbWrapper.java
@@ -0,0 +1,174 @@
+package com.danvelazco.fbwrapper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class FbWrapper extends Activity {
+	
+	private WebView fbWrapper;
+	
+	private String USERAGENT_ANDROID_DEFAULT;
+	
+	private ProgressBar mProgressBar;
+	
+	private boolean V = !Constants.PRODUCTION;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.webview);
+        
+        mProgressBar = (ProgressBar)findViewById(R.id.progress_bar);
+        
+        CookieSyncManager.createInstance(this);
+        
+        fbWrapper = (WebView) findViewById(R.id.webview);
+        fbWrapper.setWebViewClient(new FbWebViewClient());
+        fbWrapper.setWebChromeClient(new FbWebChromeClient());
+        fbWrapper.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        
+        WebSettings webSettings = fbWrapper.getSettings(); 
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSavePassword(false);
+        webSettings.setSaveFormData(false);
+        webSettings.setSupportZoom(false);
+        USERAGENT_ANDROID_DEFAULT = webSettings.getUserAgentString();
+        
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	
+    	setMobileUserAgent();
+        //setDesktopUserAgent();
+    	
+    	CookieSyncManager.getInstance().startSync();
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	
+    	CookieSyncManager.getInstance().stopSync();
+    }
+ 
+    private class FbWebChromeClient extends WebChromeClient {
+    	@Override
+    	public void onProgressChanged(WebView view, int progress) {
+    		mProgressBar.setProgress(progress);
+    	}
+    }
+    
+    private class FbWebViewClient extends WebViewClient {
+        
+    	@Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        	
+        	String domain = Uri.parse(url).getHost();
+        	
+        	//Output URL
+        	if (V) Log.d(Constants.TAG, "URL: " + url);
+        	
+        	// Let this WebView load the page. Do not override it.
+            if (domain.equals("m.facebook.com")) {
+                return false;
+            } else if (domain.equals("facebook.com")) {
+            	return false;
+            }
+            
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+        
+    	@Override
+    	public void onPageStarted(WebView view, String url, Bitmap favicon) {
+    		super.onPageStarted(view, url, favicon);
+    		
+    		mProgressBar.setVisibility(View.VISIBLE);
+    	}
+    	
+    	@Override
+    	public void onPageFinished (WebView view, String url) {
+    		super.onPageFinished(view, url);
+    		
+    		mProgressBar.setVisibility(View.INVISIBLE);
+    		
+    		CookieSyncManager.getInstance().sync();
+    	}
+    	
+    }
+    
+    private void setMobileUserAgent() {
+    	fbWrapper.getSettings().setUserAgentString(USERAGENT_ANDROID_DEFAULT);
+    	fbWrapper.loadUrl("https://m.facebook.com");
+    }
+    
+//    private void setDesktopUserAgent() {
+//    	fbWrapper.getSettings().setUserAgentString(Constants.USER_AGENT_DESKTOP);
+//    	fbWrapper.loadUrl("https://www.facebook.com");
+//    }
+    
+    private void loadNotificationsView() {
+    	fbWrapper.loadUrl("https://m.facebook.com/notifications.php");
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        
+    	// Check if the key event was the BACK key and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && fbWrapper.canGoBack()) {
+        	fbWrapper.goBack();
+            return true;
+        }
+        
+        return super.onKeyDown(keyCode, event);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu, menu);
+    	return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	
+    	switch (item.getItemId()) {
+    		case R.id.menu_notifications:
+    			loadNotificationsView();
+    			return true;
+    		case R.id.menu_preferences:
+    			return true;
+    		case R.id.menu_about:
+    			return true;
+    		case R.id.menu_exit:
+    			finish();
+    			return true;
+    	}
+    
+    	return false;
+    }
+    
+}
