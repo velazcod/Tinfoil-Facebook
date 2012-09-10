213fb28b9ff4b413df3bf94edb4be5e8a7f3a686
Daniel Velazco <velazcod@gmail.com>
2012-09-10 19:11:27 -0400

    Fix indentation

--- src/com/danvelazco/fbwrapper/Constants.java
+++ src/com/danvelazco/fbwrapper/Constants.java
@@ -10,35 +10,35 @@ public interface Constants {
 
 	/** TAG used for Logging */
 	public static final String LOG_TAG = "FB-Wrapper";
-	
+
 	/** Milliseconds to wait to hide the action bar after it was shown again */
 	public static final long ACTION_BAR_HIDE_TIMEOUT = 5000;
-	
+
 	public static final long ACTION_BAR_SHOW_FLING_SPEED = 2500;
-	
+
 	/** Ability to cleanup/destroy the web view some time after exiting the app */
 	public static final int REQUEST_WEB_VIEW_CLEANUP = 0x1000;
 	public static final long REQUEST_WEB_VIEW_CLEANUP_TIMEOUT = 120000;
-	
+
 	/** Desktop user agent (Google Chrome's user agent from a MacBook running 10.7.2, whatever) */ 
 	public static final String USER_AGENT_DESKTOP = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) "
 			+ "AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1";
 
 	/** Mobile user agent (Mobile user agent from a Google Nexus S running Android 2.3.3, whatever) */ 
 	public static final String USER_AGENT_MOBILE = "Mozilla/5.0 (Linux; U; Android 2.3.3; en-gb; Nexus S Build/GRI20) "
-			+ "AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+		+ "AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
 
 	/** URLs for Mobile site */
 	public static final String URL_MOBILE_SITE = "https://touch.facebook.com";
 	public static final String URL_MOBILE_NOTIFICATIONS = "https://touch.facebook.com/notifications.php";
-	
+
 	/** URLs for Desktop site */
 	public static final String URL_DESKTOP_SITE = "https://www.facebook.com";
 	public static final String URL_DESKTOP_NOTIFICATIONS = "https://www.facebook.com/notifications.php";
-	
+
 	/** URL for Sharing Links */
 	public static final String URL_SHARE_LINKS = "http://www.facebook.com/sharer.php?u=%s&t=%s"; //u = url & t = title
-		
+
 	/** Preferences */
 	public final static String PREFS_CAT_GENERAL = "pref_cat_general";
 	public final static String PREFS_ALLOW_CHECKINS = "prefs_allow_checkins";
@@ -50,5 +50,5 @@ public interface Constants {
 	public final static String PREFS_LOGCAT_ENABLED = "pref_logcat";
 	public final static String PREFS_HIDE_AB = "pref_hide_ab";
 	public final static String PREFS_ABOUT = "pref_about";
-
+	
 }
--- src/com/danvelazco/fbwrapper/Preferences.java
+++ src/com/danvelazco/fbwrapper/Preferences.java
@@ -13,51 +13,50 @@ import android.preference.PreferenceScreen;
 import android.view.MenuItem;
 
 public class Preferences extends PreferenceActivity implements Constants {
-
+	
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.main_preferences);
-		
+
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			/* Only mess with ActionBar if device is on honeycomb or higher */
 			ActionBar actionBar = getActionBar();
-		    actionBar.setDisplayHomeAsUpEnabled(true);
+			actionBar.setDisplayHomeAsUpEnabled(true);
 		} else {
 			/* Hide ActionBar preference if device is before Honeycomb */
 			CheckBoxPreference hideAbCheckboxPref = (CheckBoxPreference) findPreference(PREFS_HIDE_AB);
 			PreferenceCategory generalCategory = (PreferenceCategory) findPreference(PREFS_CAT_GENERAL);
 			generalCategory.removePreference(hideAbCheckboxPref);
 		}
-		
+
 	}
 	
 	@Override
 	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-		
+
 		String key = preference.getKey();
 		
-		if (PREFS_ABOUT.equals(key)) {
+		if (PREFS_ABOUT.equals(key))
 			showAboutAlert();
-		}
-		
+
 		return true;
 	}
-	
+
 	private void showAboutAlert() {
-    	AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle(getString(R.string.menu_about));
-        alertDialog.setMessage(getString(R.string.txt_about));
-        alertDialog.setIcon(R.drawable.ic_launcher); 
-        alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, getString(R.string.lbl_dialog_close), 
-        		new DialogInterface.OnClickListener() {
-        	public void onClick(DialogInterface dialog, int which) {
-        		return;
-        	} 
-        });
-        alertDialog.show();
-    }
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle(getString(R.string.menu_about));
+		alertDialog.setMessage(getString(R.string.txt_about));
+		alertDialog.setIcon(R.drawable.ic_launcher); 
+		alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, getString(R.string.lbl_dialog_close), 
+				new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				return;
+				} 
+		});
+		alertDialog.show();
+	}
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -69,5 +68,5 @@ public class Preferences extends PreferenceActivity implements Constants {
 				return super.onOptionsItemSelected(item);
 		}
 	}
-	
+
 }
