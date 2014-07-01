9220107f7f6f08a2a5c4b6bafdd37d228f9b3cd9
Daniel Velazco <velazcod@gmail.com>
2014-07-01 11:39:07 -0700

    Hack to go back when opening an external site is no longer needed since Facebook has fixed the linker's bad behavior on the mobile site.
    Fixes #16

--- Tinfoil-for-Facebook/src/main/java/com/danvelazco/fbwrapper/activity/BaseFacebookWebViewActivity.java
+++ Tinfoil-for-Facebook/src/main/java/com/danvelazco/fbwrapper/activity/BaseFacebookWebViewActivity.java
@@ -491,13 +491,6 @@ public abstract class BaseFacebookWebViewActivity extends Activity implements
         // that handles this URL
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(intent);
-
-        // Hack: Facebook uses a linker helper, it's blank when coming back to app
-        // from an outside link, so let's attempt to go back to avoid this blank page
-        if (mWebView.canGoBack()) {
-            Logger.d(LOG_TAG, "Attempting to go back to avoid blank page");
-            mWebView.goBack();
-        }
     }
 
     /**
